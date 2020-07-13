75b65969f7896b44eb11902079eb33094ae39817
AbdulMalik <abdumaliksharipov@gmail.com>
2020-07-13 23:59:23 +0500

    Sepereating files

--- app.js
+++ app.js
@@ -1,52 +1,5 @@
 
 
-class Timer {
-    constructor(durationInput, startBtn,pauseBtn,callbacks){
-        this.durationInput=durationInput;
-        this.startBtn=startBtn;
-        this.pauseBtn=pauseBtn;
-
-        if(callbacks){
-            this.onStart=callbacks.onStart;
-        }
-
-        this.startBtn.addEventListener("click",this.start);
-        this.pauseBtn.addEventListener("click",this.pause);
-    }
-
-    start = () =>{
-        if(this.onStart){
-            this.onStart();
-        }
-        this.tick();
-        this.intervalId=setInterval(this.tick,1000);
-        
-    }
-
-    tick =()=>{
-        if(this.timeRemaining<=0){
-            this.pause();
-        }else{
-
-            this.timeRemaining = this.timeRemaining-1;
-        }
-        
-    }
-
-    pause =()=>{
-        clearInterval(this.intervalId);
-    }
-
-    get timeRemaining(){
-        return parseFloat(this.durationInput.value)
-    }
-
-    set timeRemaining(time){
-        this.durationInput.value=time;
-    }
-}
-
-
 const durationInput=document.querySelector("#duration");
 const startBtn=document.querySelector("#start");
 const pauseBtn=document.querySelector("#pause");
@@ -59,10 +12,13 @@ const timer = new Timer(durationInput,startBtn,pauseBtn,{
     },
 
     onTick(){
+        console.log("Timer just ticked down");
+        
 
     },
 
     onComplete(){
+        console.log("Timer is completed");        
 
     }
 });
